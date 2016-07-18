67da5405532d3d6c48cb128a43014fc8f520887d
Aaron Iker <aaron.iker@web.de>
2016-07-19 00:34:06 +0200

    add button & table check

--- admin/view/user/list.php
+++ admin/view/user/list.php
@@ -2,11 +2,21 @@
 
     <header>
 
-        <h2><?=lang::get('user'); ?></h2>
+        <h2><?=lang::get('list'); ?></h2>
 
         <div class="search">
             <input type="text" v-model="searchString">
         </div>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="" class="button">
+                        <?=lang::get('add'); ?>
+                    </a>
+                </li>
+            </ul>
+        </nav>
 
         <div class="clear"></div>
 
