bbb6e6bb748fe0438fa64e72ee426e73c1dca2e8
Aaron Iker <aaron.iker@web.de>
2016-07-18 18:42:19 +0200

    spaces

--- gear/functions/html.php
+++ gear/functions/html.php
@@ -1,64 +1,64 @@
 <?php
-		
-	function html_convertAttribute($attributes) {
-		
-		$return = '';
-			
-		foreach($attributes as $key => $val) {
-				
-			if(is_int($key)) {
-					
-				$return .= ' '.$val;
-					
-			} else {
-					
-				if(is_array($val)) {
-					$val = implode(' ', $val);	
-				}
-				
-				$return .= ' '.htmlspecialchars($key).'="'.htmlspecialchars($val).'"';	
-				
-			}			
-			
-		}
-			
-		return $return;
-		
-	}
-	
-	function generateRandomString($length = 10) {
-    
-		return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
-	
-	}
-	
-	function getDeleteModal($title, $content) {
-	
-		if($content == '') {
-			$content = lang::get('really_delete');	
-		}
-		
-		if($title == '') {
-			$title = lang::get('really_delete');	
-		}
-		
-	?>
-	<div class="modal fade" id="delete_modal" tabindex="-1" role="dialog" aria-hidden="true">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					<h3 class="modal-title"><?= $title ?></h3>
-				</div>
-				<div class="modal-body"><?= $content; ?></div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-sm btn-default" data-dismiss="modal"><?= lang::get('close') ?></button>
-					<button type="button" class="btn btn-sm btn-danger confirm"><?= lang::get('delete') ?></button>
-				</div>
-			</div>
-		</div>
-	</div>
-	<?php
-	}
+
+    function html_convertAttribute($attributes) {
+
+        $return = '';
+
+        foreach($attributes as $key => $val) {
+
+            if(is_int($key)) {
+
+                $return .= ' '.$val;
+
+            } else {
+
+                if(is_array($val)) {
+                    $val = implode(' ', $val);
+                }
+
+                $return .= ' '.htmlspecialchars($key).'="'.htmlspecialchars($val).'"';
+
+            }
+
+        }
+
+        return $return;
+
+    }
+
+    function generateRandomString($length = 10) {
+
+        return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
+
+    }
+
+    function getDeleteModal($title, $content) {
+
+        if($content == '') {
+            $content = lang::get('really_delete');
+        }
+
+        if($title == '') {
+            $title = lang::get('really_delete');
+        }
+
+    ?>
+    <div class="modal fade" id="delete_modal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h3 class="modal-title"><?= $title ?></h3>
+                </div>
+                <div class="modal-body"><?= $content; ?></div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-sm btn-default" data-dismiss="modal"><?= lang::get('close') ?></button>
+                    <button type="button" class="btn btn-sm btn-danger confirm"><?= lang::get('delete') ?></button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    }
 
 ?>
--- gear/functions/validate.php
+++ gear/functions/validate.php
@@ -1,47 +1,47 @@
 <?php
-	
-	function validate($data, $addRules = []) {
-		
-		$rules = [
-			'email' => 'valid_email'
-		];
-		
-		$rulesArray = array_merge($rules, $addRules);
-		
-		validate::set_field_name("email", lang::get('email'));
-		validate::set_field_name("password", lang::get('password'));
-		
-		$validate = new validate();
-		
-		$validate->validation_rules($rulesArray);
-		
-		$is_valid = ($validate->run($data) === false) ? false : true;
-		
-		return [$is_valid, $validate->get_readable_errors()];
-		
-	}
-	
-	function file_rename($path, $filename) {
 
-		if($pos = strrpos($filename, '.')) {
-			$name = substr($filename, 0, $pos);
-			$ext = substr($filename, $pos);
-		} else {
-			$name = $filename;
-		}
-		
-		$newpath = $path.'/'.$filename;
-		$newname = $filename;
-		$counter = 0;
-		
-		while (file_exists($newpath)) {
-			$newname = $name .'_'. $counter . $ext;
-			$newpath = $path.'/'.$newname;
-			$counter++;
-		}
-		
-		return $newname;
-	
-	}
-	
+    function validate($data, $addRules = []) {
+
+        $rules = [
+            'email' => 'valid_email'
+        ];
+
+        $rulesArray = array_merge($rules, $addRules);
+
+        validate::set_field_name("email", lang::get('email'));
+        validate::set_field_name("password", lang::get('password'));
+
+        $validate = new validate();
+
+        $validate->validation_rules($rulesArray);
+
+        $is_valid = ($validate->run($data) === false) ? false : true;
+
+        return [$is_valid, $validate->get_readable_errors()];
+
+    }
+
+    function file_rename($path, $filename) {
+
+        if($pos = strrpos($filename, '.')) {
+            $name = substr($filename, 0, $pos);
+            $ext = substr($filename, $pos);
+        } else {
+            $name = $filename;
+        }
+
+        $newpath = $path.'/'.$filename;
+        $newname = $filename;
+        $counter = 0;
+
+        while (file_exists($newpath)) {
+            $newname = $name .'_'. $counter . $ext;
+            $newpath = $path.'/'.$newname;
+            $counter++;
+        }
+
+        return $newname;
+
+    }
+
 ?>
