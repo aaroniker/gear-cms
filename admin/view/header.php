9223ee7f3d1384b460e2fdddcacaa1316c2e0aec
Aaron Iker <aaron.iker@web.de>
2016-07-18 18:32:44 +0200

    spaces

--- admin/view/header.php
+++ admin/view/header.php
@@ -1,113 +1,109 @@
 <header id="head">
 
-  <?php
-    $menu = admin::getMenu();
+    <?php
+        $menu = admin::getMenu();
+        if($menu):
+    ?>
 
-    if($menu):
-  ?>
+    <div class="menu">
+        <div class="container">
+            <nav>
+                <ul>
+                    <?php
+                        foreach($menu as $url => $array) {
 
-  <div class="menu">
+                            $class = ($array['class']) ? ' class="'.$array['class'].'"' : '';
+
+                            echo '
+                                <li'.$class.'>
+                                    <a href="'.config::get('url').'admin/'.$url.'">'.$array['name'].'</a>
+                                </li>
+                            ';
+
+                        }
+                    ?>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+    <?php
+        endif;
+    ?>
+
     <div class="container">
-      <nav>
-        <ul>
-          <?php
 
-            foreach($menu as $url => $array) {
+        <h1><?=admin::$page; ?></h1>
 
-              $class = ($array['class']) ? ' class="'.$array['class'].'"' : '';
+        <div class="user">
 
-              echo '
-                <li'.$class.'>
-                  <a href="'.config::get('url').'admin/'.$url.'">'.$array['name'].'</a>
-                </li>
-              ';
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">
+                            <i class="icon icon-ios-help-outline"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?=config::get('url'); ?>">
+                            <i class="icon icon-ios-home-outline"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="?logout=1">
+                            <i class="icon icon-log-in"></i>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
 
-            }
-
-          ?>
-        </ul>
-      </nav>
-    </div>
-  </div>
-
-  <?php
-    endif;
-  ?>
-
-  <div class="container">
-
-    <h1><?=admin::$page; ?></h1>
-
-    <div class="user">
-
-      <nav>
-        <ul>
-          <li>
-            <a href="">
-              <i class="icon icon-ios-help-outline"></i>
+            <a href="" class="profile">
+                <img src="<?=config::get('url').'admin/assets/img/avatar.jpg'; ?>" alt="">
+                <?=user::current()->email; ?>
             </a>
-          </li>
-          <li>
-            <a href="<?=config::get('url'); ?>">
-              <i class="icon icon-ios-home-outline"></i>
-            </a>
-          </li>
-          <li>
-            <a href="?logout=1">
-              <i class="icon icon-log-in"></i>
-            </a>
-          </li>
-        </ul>
-      </nav>
 
-      <a href="" class="profile">
-        <img src="<?=config::get('url').'admin/assets/img/avatar.jpg'; ?>" alt="">
-        <?=user::current()->email; ?>
-      </a>
+            <div class="clear"></div>
 
-      <div class="clear"></div>
+        </div>
+
+        <div class="clear"></div>
 
     </div>
 
-    <div class="clear"></div>
+    <?php
+        $submenu = admin::getSubmenu();
 
-  </div>
+        if($submenu):
+    ?>
 
-  <?php
-    $submenu = admin::getSubmenu();
+    <div class="submenu">
+        <div class="container">
+            <nav>
+                <ul>
+                    <?php
 
-    if($submenu):
-  ?>
+                        foreach($submenu as $url => $array) {
 
-  <div class="submenu">
-    <div class="container">
-      <nav>
-        <ul>
-          <?php
+                            $class = ($array['class']) ? ' class="'.$array['class'].'"' : '';
 
-            foreach($submenu as $url => $array) {
+                            echo '
+                                <li'.$class.'>
+                                    <a href="'.config::get('url').'admin/'.$url.'">'.$array['name'].'</a>
+                                </li>
+                            ';
 
-              $class = ($array['class']) ? ' class="'.$array['class'].'"' : '';
+                        }
 
-              echo '
-                <li'.$class.'>
-                  <a href="'.config::get('url').'admin/'.$url.'">'.$array['name'].'</a>
-                </li>
-              ';
-
-            }
-
-          ?>
-        </ul>
-      </nav>
+                    ?>
+                </ul>
+            </nav>
+        </div>
     </div>
-  </div>
 
-  <?php
-    endif;
-  ?>
+    <?php
+        endif;
+    ?>
 
 </header>
 
 <div class="container">
-  <main>
