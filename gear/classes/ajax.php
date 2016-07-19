a85fb7e5350794ca6df2b3f419898243538ffa26
Aaron Iker <aaron.iker@web.de>
2016-07-19 11:25:27 +0200

    lines

--- gear/classes/ajax.php
+++ gear/classes/ajax.php
@@ -5,20 +5,15 @@ class ajax {
     static $return = [];
 
     public static function is() {
-
         return (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
-
     }
 
     public static function addReturn($text) {
         self::$return[] = $text;
-
     }
 
     public static function getReturn() {
-
         return implode('<br />', self::$return);
-
     }
 
 }
