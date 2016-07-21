06959b35375ea8173a2dfa7ddee1c55d47e8dafd
Aaron Iker <aaron.iker@web.de>
2016-07-21 16:57:26 +0200

    add file_size function

--- gear/functions/validate.php
+++ gear/functions/validate.php
@@ -44,4 +44,15 @@
 
     }
 
+    function file_size($path) {
+
+        $size = filesize($path);
+
+        $units = array( 'B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+        $power = $size > 0 ? floor(log($size, 1024)) : 0;
+
+        return number_format($size / pow(1024, $power), 2, '.', ',') . ' ' . $units[$power];
+
+    }
+
 ?>
