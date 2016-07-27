9333a3a758de3ba91b2c779d5c5b947c822af116
Aaron Iker <aaron.iker@web.de>
2016-07-27 14:53:02 +0200

    add user function

--- admin/view/user/add.php
+++ admin/view/user/add.php
@@ -1,7 +1,5 @@
 <section id="user">
 
-    <form class="horizontal">
-
     <header>
 
         <h2><?=lang::get('add'); ?></h2>
@@ -13,37 +11,54 @@
                         <i class="icon icon-close-round"></i>
                     </a>
                 </li>
-                <li>
-                    <button type="submit" class="button">
-                        <?=lang::get('save'); ?>
-                    </button>
-                </li>
             </ul>
         </nav>
 
     </header>
 
+    <?php
+
+        $form = new form();
+
+        $field = $form->addTextField('email', '');
+	    $field->fieldName(lang::get('email'));
+        $field->fieldValidate('valid_email|required');
+
+	    $field = $form->addPasswordField('password', '');
+        $field->fieldName(lang::get('password'));
+        $field->fieldValidate();
+
+        if($form->isSubmit()) {
+
+            if($form->validation()) {
+
+			    extension::add('model_beforeInsert', function($data) {
+    			    $password = password_hash($data['password'], PASSWORD_DEFAULT);
+    			    $data['password'] = $password;
+    		        return $data;
+			    });
+
+			    $this->model->insert($form->getAll());
+
+    			echo message::success(lang::get('user_added'));
+
+		    } else {
+			    echo $form->getErrors();
+		    }
+
+	    }
+
+    ?>
+
     <div class="columns">
         <div class="md-6">
 
-            <div class="form-element">
-                <label for="email" class="sm-3"><?=lang::get('email'); ?></label>
-                <div class="sm-9">
-                    <input type="text" name="email" class="form-field" id="email">
-                </div>
-            </div>
-
-            <div class="form-element">
-                <label for="password" class="sm-3"><?=lang::get('password'); ?></label>
-                <div class="sm-9">
-                    <input type="password" name="password" class="form-field" id="password">
-                </div>
-            </div>
+            <?php
+                echo $form->show();
+            ?>
 
         </div>
 
     </div>
-
-    </form>
 
 </section>
