4b80f44b427ae0615c27fe6c3833f5ec8e532a00
Aaron Iker <aaron.iker@web.de>
2016-08-01 23:54:51 +0200

    prepare permission structure

--- admin/index.php
+++ admin/index.php
@@ -12,14 +12,15 @@
     theme::addJS('https://cdn.jsdelivr.net/vue/1.0.26/vue.min.js');
     theme::addJS('admin/assets/js/app.js', true);
 
-    userPerm::add('content[manage]', lang::get('content[manage]'));
-    userPerm::add('user[manage]', lang::get('user[manage]'));
-    userPerm::add('user[manage][add]', lang::get('user[manage][add]'));
-    userPerm::add('user[manage][edit]', lang::get('user[manage][edit]'));
-    userPerm::add('user[manage][delete]', lang::get('user[manage][delete]'));
-    userPerm::add('plugins[manage]', lang::get('plugins[manage]'));
-    userPerm::add('plugins[manage][install]', lang::get('plugins[manage][install]'));
-    userPerm::add('plugins[manage][delete]', lang::get('plugins[manage][delete]'));
+    userPerm::add('content[index]', lang::get('content[index]'));
+    userPerm::add('content[storage]', lang::get('content[storage]'));
+    userPerm::add('user[index]', lang::get('user[index]'));
+    userPerm::add('user[index][add]', lang::get('user[index][add]'));
+    userPerm::add('user[index][edit]', lang::get('user[index][edit]'));
+    userPerm::add('user[index][delete]', lang::get('user[index][delete]'));
+    userPerm::add('plugins[index]', lang::get('plugins[index]'));
+    userPerm::add('plugins[index][install]', lang::get('plugins[index][install]'));
+    userPerm::add('plugins[index][delete]', lang::get('plugins[index][delete]'));
 
     ob_start();
 
