6de91e12023b6cb2148ef8e23314f2f92d0df99b
Aaron Iker <aaron.iker@web.de>
2016-08-02 11:47:08 +0200

    modal demo / rights

--- admin/view/user/rights/list.php
+++ admin/view/user/rights/list.php
@@ -1,5 +1,32 @@
-<pre>
+<section id="rights">
+
+    <header>
+
+        <h2><?=lang::get('rights'); ?></h2>
+
+    </header>
+
+    <button @click="showModal = true">Show Modal</button>
+
+    <modal :show.sync="showModal">
+        <h3 slot="header">custom header</h3>
+    </modal>
+
+    <pre>
+        <?php
+            var_dump(userPerm::getAll());
+        ?>
+    </pre>
+
+</section>
+
 <?php
-    var_dump(userPerm::getAll());
+    theme::addJSCode('
+        new Vue({
+            el: "#rights",
+            data: {
+                showModal: false
+            }
+        });
+    ');
 ?>
-</pre>
