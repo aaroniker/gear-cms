b0459a69cc4ef3c715b84521ff46fac98158baaa
Aaron Iker <aaron.iker@web.de>
2016-08-19 12:32:40 +0200

    session_destroy

--- gear/classes/user/session.php
+++ gear/classes/user/session.php
@@ -3,11 +3,13 @@
 class userSession {
 
     public static function init() {
-
         if(session_id() == '') {
             session_start();
         }
+    }
 
+    public static function destroy() {
+        session_destroy();
     }
 
 }
