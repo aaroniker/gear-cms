66e1dc930b4e68490b98cfb5e69352eb1dd4eeb8
Aaron Iker <aaron.iker@web.de>
2016-08-20 02:22:55 +0200

    toggleNav function outside ready()

--- admin/assets/js/layout.js
+++ admin/assets/js/layout.js
@@ -1,33 +1,5 @@
 $(document).ready(function() {
 
-    function toggleNav(open) {
-
-        var expand = $(this);
-        var overlay = $("#overlay");
-        var nav = $("#nav");
-        var active = expand.hasClass('active');
-
-        expand.toggleClass('active');
-
-        if(open) {
-            $("body").addClass('fix');
-            overlay.fadeIn(200, function() {
-                nav.animate({
-                    left: 0
-                }, 200);
-            });
-        } else {
-            nav.animate({
-                left: -220
-            }, 200, function() {
-                overlay.fadeOut(200, function() {
-                    $("body").removeClass('fix');
-                });
-            });
-        }
-
-    }
-
     $("#head .expand").click(function() {
         toggleNav(!$(this).hasClass('active'));
     });
@@ -51,4 +23,32 @@ function getMessages(url) {
             }
         }
     });
+}
+
+function toggleNav(open) {
+
+    var expand = $(this);
+    var overlay = $("#overlay");
+    var nav = $("#nav");
+    var active = expand.hasClass('active');
+
+    expand.toggleClass('active');
+
+    if(open) {
+        $("body").addClass('fix');
+        overlay.fadeIn(200, function() {
+            nav.animate({
+                left: 0
+            }, 200);
+        });
+    } else {
+        nav.animate({
+            left: -220
+        }, 200, function() {
+            overlay.fadeOut(200, function() {
+                $("body").removeClass('fix');
+            });
+        });
+    }
+
 }
