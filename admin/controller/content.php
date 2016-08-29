8499617a91c4490c3b9f7a11c1ee575d27e20cdf
Aaron Iker <aaron.iker@web.de>
2016-08-29 13:17:05 +0200

    addDir method

--- admin/controller/content.php
+++ admin/controller/content.php
@@ -10,13 +10,34 @@ class contentController extends controller {
 
     }
 
-    public function media() {
+    public function media($action = '') {
 
         if(ajax::is()) {
 
             $path = type::post('path', 'string', '');
 
-            ajax::addReturn(json_encode(file_list($path)));
+            if($action == 'get') {
+
+                ajax::addReturn(json_encode(file_list($path)));
+
+            } elseif($action == 'addDir') {
+
+                $name = type::post('name', 'string', '');
+
+                $path = dir::media($path.$name);
+
+                if($name) {
+
+                    if(!file_exists($path)) {
+                        mkdir($path, 0777, true);
+                        message::success(lang::get('dir_added').$path);
+                    } else {
+                        message::error(lang::get('dir_exists').$path);
+                    }
+
+                }
+
+            }
 
         }
 
--- admin/view/content/media/list.php
+++ admin/view/content/media/list.php
@@ -11,7 +11,7 @@
         <nav>
             <ul>
                 <li>
-                    <a href="" class="button border">
+                    <a @click="addDirModal = true" class="button border">
                         <?=lang::get('add_dir'); ?>
                     </a>
                 </li>
@@ -24,6 +24,27 @@
         </nav>
 
     </header>
+
+    <?php
+
+        $form = new form();
+        $form->setHorizontal(false);
+
+        $form->addFormAttribute('v-on:submit.prevent', 'addDir');
+
+        $field = $form->addTextField('name', '');
+        $field->fieldName(lang::get('name'));
+        $field->addAttribute('v-model', 'dirName');
+        $field->fieldValidate();
+
+    ?>
+
+    <modal :show.sync="addDirModal">
+        <h3 slot="header"><?=lang::get('add_dir'); ?></h3>
+        <div slot="content">
+            <?=$form->show(); ?>
+        </div>
+    </modal>
 
     <file-table :data="tableData" :columns="['name', 'size']" :headline="headline" :filter-key="search"></data-table>
 
@@ -39,15 +60,42 @@ theme::addJSCode('
             path: "/",
             tableData: '.json_encode(file_list('/')).',
             search: "",
-            showSearch: true
+            showSearch: true,
+            addDirModal: false,
+            dirName: ""
         },
         events: {
-            checked: function (data) {
+            checked: function(data) {
                 this.checked = data;
             },
-            headline: function (data) {
+            path: function(data) {
+                this.path = data;
+            },
+            headline: function(data) {
                 this.headline = data.headline;
                 this.showSearch = data.showSearch;
+            }
+        },
+        methods: {
+            addDir: function() {
+
+                var vue = this;
+
+                $.ajax({
+                    method: "POST",
+                    url: "'.url::admin('content', ['media', 'addDir']).'",
+                    dataType: "text",
+                    data: {
+                        name: vue.dirName,
+                        path: vue.path
+                    },
+                    success: function(data) {
+                        vue.$broadcast("fetchData");
+                        vue.addDirModal = false;
+                        vue.dirName = "";
+                    }
+                });
+
             }
         }
     });
