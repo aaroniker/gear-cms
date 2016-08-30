9a0f31c69c9340585bd672736511618a11856d3d
Aaron Iker <aaron.iker@web.de>
2016-08-30 13:26:51 +0200

    simple file size

--- gear/classes/media.php
+++ gear/classes/media.php
@@ -32,7 +32,7 @@ class media {
         $units = ['B', 'KB', 'MB', 'GB', 'TB'];
         $power = ($size > 0) ? floor(log($size, 1024)) : 0;
 
-        return number_format($size / pow(1024, $power), 2, '.', ',') . ' ' . $units[$power];
+        return number_format($size / pow(1024, $power), 0, '.', ',') . ' ' . $units[$power];
 
     }
 
