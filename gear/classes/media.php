dd997cfa7f8210123b2205908b47eb464cda89b0
Aaron Iker <aaron.iker@web.de>
2016-09-05 15:32:03 +0200

    add media class max file size methods

--- gear/classes/media.php
+++ gear/classes/media.php
@@ -136,6 +136,34 @@ class media {
 
     }
 
+    public static function getServerMaxSize() {
+
+        static $max_size = -1;
+
+        if($max_size < 0) {
+            $max_size = self::parseSize(ini_get('post_max_size'));
+
+            $upload_max = self::parseSize(ini_get('upload_max_filesize'));
+
+            if($upload_max > 0 && $upload_max < $max_size) {
+                $max_size = $upload_max;
+            }
+        }
+
+        return $max_size;
+
+    }
+
+    public static function parseSize($size) {
+        $unit = preg_replace('/[^bkmgtpezy]/i', '', $size);
+        $size = preg_replace('/[^0-9\.]/', '', $size);
+        if($unit) {
+            return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+        } else {
+            return round($size);
+        }
+    }
+
 }
 
 ?>
