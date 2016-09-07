68b722a43f656b390e81dd9d7079b8ea131e278d
Aaron Iker <aaron.iker@web.de>
2016-09-07 18:40:00 +0200

    gearUpload.js cdn

--- admin/index.php
+++ admin/index.php
@@ -8,8 +8,8 @@
 
     theme::addJS('https://cdn.jsdelivr.net/vue/1.0.26/vue.js');
     theme::addJS('https://cdn.jsdelivr.net/jquery/3.1.0/jquery.min.js');
+    theme::addJS('https://cdn.jsdelivr.net/gear-upload/0.2/gearUpload.min.js');
     theme::addJS(url::assets('js/session.js'));
-    theme::addJS(url::assets('js/gearUpload.js'));
     theme::addJS(url::assets('js/app.js'));
     theme::addJS(url::assets('js/layout.js'));
 
