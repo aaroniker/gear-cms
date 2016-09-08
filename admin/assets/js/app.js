066a4a6fd4129d91bd12d762ed4b3fb2f5b35050
Aaron Iker <aaron.iker@web.de>
2016-09-08 11:03:10 +0200

    store directives extra js file

--- admin/assets/js/app.js
+++ admin/assets/js/app.js
@@ -6,22 +6,6 @@ Vue.filter("lang", function(value) {
     }
 });
 
-Vue.directive("slot", {
-    bind: function() {
-        var host = this.vm;
-        var root = host.$root;
-        var raw = host._context._directives.filter(function(value) {
-            return !(value.Component === undefined);
-        })[0].el;
-
-        for(var i = 0; i < raw.children.length; i++) {
-            var node = raw.children[i].cloneNode(true);
-            this.el.appendChild(node);
-            root.$compile(node, host, this._scope);
-        }
-    }
-});
-
 Vue.component("table-cell", {
     template: '<td :class="class"><slot></slot></td>',
     props: {
