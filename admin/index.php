3723e96de0dc011cd0ab7e8faf74f06ae5dc8767
Aaron Iker <aaron.iker@web.de>
2016-09-16 13:00:14 +0200

    add sortable js

--- admin/index.php
+++ admin/index.php
@@ -10,6 +10,7 @@
     theme::addJS('https://cdn.jsdelivr.net/jquery/3.1.0/jquery.min.js');
     theme::addJS('https://cdn.jsdelivr.net/gear-upload/0.2/gearUpload.min.js');
     theme::addJS(url::assets('js/session.js'));
+    theme::addJS(url::assets('js/sortable.js'));
     theme::addJS(url::assets('js/directives.js'));
     theme::addJS(url::assets('js/app.js'));
     theme::addJS(url::assets('js/layout.js'));
