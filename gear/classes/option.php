d2c583a3e3d94abc786ba58044747b77de3a7512
Aaron Iker <aaron.iker@web.de>
2016-09-19 12:17:42 +0200

    add option class

---
+++ gear/classes/option.php
@@ -0,0 +1,19 @@
+<?php
+
+class option {
+
+    public static function has($name) {
+
+    }
+
+    public static function get($name, $default = null) {
+
+    }
+
+    public static function set($name, $value) {
+
+    }
+
+}
+
+?>
