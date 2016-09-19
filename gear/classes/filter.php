1137468b3ce7be7efc8dbff3b2634d15b333fe68
Aaron Iker <aaron.iker@web.de>
2016-09-19 14:26:26 +0200

    filter - umlauts replace

--- gear/classes/filter.php
+++ gear/classes/filter.php
@@ -2,6 +2,8 @@
 
 class filter {
 
+    protected static $umlauts = ['Ä', 'ä', 'Ö', 'ö', 'Ü', 'ü', 'ß'];
+    protected static $umlautsReplace = ['Ae', 'ae', 'Oe', 'oe', 'Ue', 'ue', 'ss'];
     protected static $accents = ['Š' => 'S', 'š' => 's', 'Ð' => 'Dj','Ž' => 'Z', 'ž' => 'z', 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C', 'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Þ' => 'B', 'ß' => 'Ss','à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'a', 'ç' => 'c', 'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'o', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ý' => 'y', 'ý' => 'y', 'þ' => 'b', 'ÿ' => 'y', 'ƒ' => 'f'];
 
     public static function xss($string) {
@@ -16,6 +18,7 @@ class filter {
 
     public static function url($string) {
 
+        $string = str_replace(self::$umlauts, self::$umlautsReplace, $string);
         $string = strtr($string, self::$accents);
         $string = strtolower($string);
         $string = preg_replace('/[^a-zA-Z0-9\s]/', '-', $string);
@@ -29,6 +32,7 @@ class filter {
 
     public static function file($string) {
 
+        $string = str_replace(self::$umlauts, self::$umlautsReplace, $string);
         $string = strtr($string, self::$accents);
         $string = preg_replace('/[^.a-zA-Z0-9\s]/', '-', $string);
         $string = preg_replace('{ +}', ' ', $string);
