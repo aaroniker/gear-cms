de158acf06fff57fd1b3e21eb398d1a066973a9c
Aaron Iker <aaron.iker@web.de>
2016-09-20 16:56:05 +0200

    add system/general settings

---
+++ admin/controller/system.php
@@ -0,0 +1,17 @@
+<?php
+
+class systemController extends controller {
+
+    public function __construct() {
+
+    }
+
+    public function index($action = '', $id = 0) {
+
+        include(dir::view('system/general.php'));
+
+    }
+
+}
+
+?>
