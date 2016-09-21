2cf24689e5a0db1dac0718db2ecf8b13664125c9
Aaron Iker <aaron.iker@web.de>
2016-09-21 23:19:40 +0200

    plugin class / getAll() __construct getConfig()

--- gear/classes/plugin.php
+++ gear/classes/plugin.php
@@ -2,9 +2,41 @@
 
 class plugin {
 
+    protected static $allPlugins = [];
+
     protected $config = [];
 
-    public function __construct() {
+    public function __construct($name) {
+
+        $file = dir::plugins('plugin.json', $name);
+
+        if(file_exists($file)) {
+		    $this->config = json_decode(file_get_contents($file), true);
+        }
+
+	}
+
+    public function getConfig() {
+        return $this->config;
+    }
+
+	public static function getAll() {
+
+        if(!count(self::$allPlugins)) {
+
+            $plugins = array_diff(scandir(dir::plugins()), ['.', '..']);
+
+            foreach($plugins as $dir) {
+
+                $plugin = new plugin($dir);
+
+                self::$allPlugins[] = $plugin->getConfig();
+
+            }
+
+        }
+
+		return self::$allPlugins;
 
 	}
 
