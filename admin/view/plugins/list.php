c50f884d81268970c2256f4236f663b9b7c3f9d0
Aaron Iker <aaron.iker@web.de>
2016-09-21 23:35:07 +0200

    plugin list

--- admin/view/plugins/list.php
+++ admin/view/plugins/list.php
@@ -10,9 +10,12 @@
 
     </header>
 
-    <data-table :data="tableData" :columns="['name', 'description']" :headline="headline" :filter-key="search">
+    <data-table :data="tableData" :columns="['name', 'description', '']" :headline="headline" :filter-key="search">
         <table-cell>{{ entry.name }}</table-cell>
         <table-cell>{{ entry.description }}</table-cell>
+        <table-cell class="shrink">
+            info
+        </table-cell>
     </data-table>
 
 </section>
