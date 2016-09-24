1bf8b2e0807aa915e7fd30d5a4525fcd6fb36360
Aaron Iker <aaron.iker@web.de>
2016-09-25 00:02:25 +0200

    update jquery 3.1.1

--- admin/index.php
+++ admin/index.php
@@ -7,7 +7,7 @@
     theme::addCSS(url::assets('css/style.css'));
 
     theme::addJS(url::assets('js/vue.js'));
-    theme::addJS('https://cdn.jsdelivr.net/jquery/3.1.0/jquery.min.js');
+    theme::addJS('https://cdn.jsdelivr.net/jquery/3.1.1/jquery.min.js');
     theme::addJS('https://cdn.jsdelivr.net/gear-upload/0.2/gearUpload.min.js');
     theme::addJS(url::assets('js/session.js'));
     theme::addJS(url::assets('js/sortable.js'));
