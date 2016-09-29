6f5e1e9ba1b8301d3ae1184d2cd5ede4dec08b9b
Aaron Iker <aaron.iker@web.de>
2016-09-29 16:29:12 +0200

    add chartist-tooltip

--- admin/controller/dashboard.php
+++ admin/controller/dashboard.php
@@ -6,6 +6,7 @@ class dashboardController extends controller {
 
         theme::addCSS('https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.css');
         theme::addJS('https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.js');
+        theme::addJS(url::assets('js/tooltip.js'));
 
         include(dir::view('dashboard/index.php'));
 
