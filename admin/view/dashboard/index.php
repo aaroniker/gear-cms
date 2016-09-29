b02f876176ddf7f510ece8b02a04a1929eff5096
Aaron Iker <aaron.iker@web.de>
2016-09-29 15:50:58 +0200

    prepare dashboard

--- admin/view/dashboard/index.php
+++ admin/view/dashboard/index.php
@@ -1,1 +1,21 @@
-dash
+<section id="dashboard">
+
+    <header>
+
+        <h2><?=lang::get('overview'); ?></h2>
+
+    </header>
+
+    dash
+
+</section>
+
+<?php
+theme::addJSCode('
+    new Vue({
+        el: "#dashboard",
+        data: {
+        }
+    });
+');
+?>
