e78ab035db97dedb1c0b79ea9f591123a20b3ac1
Aaron Iker <aaron.iker@web.de>
2016-10-05 16:53:01 +0200

    improve dashboard calendar js

--- admin/assets/js/calendar.js
+++ admin/assets/js/calendar.js
@@ -6,7 +6,7 @@ $(function() {
         var u = false;
         contentEl.empty();
         while(!u) {
-            if(s[r] == e[0].weekday) {
+            if(weekdayArr[r] == e[0].weekday) {
                 u = true;
             } else {
                 contentEl.append('<div class="blank"></div>');
@@ -15,14 +15,14 @@ $(function() {
         }
         for(var c = 0; c < 42 - r; c++) {
             if(c >= e.length) {
-                contentEl.append('<div class="blank"></div>')
+                contentEl.append('<div class="blank"></div>');
             } else {
                 var v = e[c].day;
                 var m = g(new Date(year, month - 1, v)) ? '<div class="today">' : "<div>";
-                contentEl.append(m + "" + v + "</div>")
+                contentEl.append(m + "" + v + "</div>");
             }
         }
-        toolbar.find("h3").text(lang[i[month - 1]] + " " + year);
+        toolbarEl.find("h3").text(lang[monthArr[month - 1]] + " " + year);
     }
 
     function h() {
@@ -30,7 +30,7 @@ $(function() {
         for(var r = 1; r < v(year, month) + 1; r++) {
             e.push({
                 day: r,
-                weekday: s[m(year, month, r)]
+                weekday: weekdayArr[m(year, month, r)]
             });
         }
         return e;
@@ -39,7 +39,7 @@ $(function() {
     function weekdays() {
         weekdaysEl.empty();
         for(var e = 0; e < 7; e++) {
-            weekdaysEl.append("<div>" + lang[s[e]].substring(0, 2) + "</div>")
+            weekdaysEl.append("<div>" + lang[weekdayArr[e]].substring(0, 2) + "</div>");
         }
     }
 
@@ -62,7 +62,7 @@ $(function() {
     function b() {
         var date = new Date;
         year = date.getFullYear();
-        month = date.getMonth() + 1
+        month = date.getMonth() + 1;
     }
 
     var e = null;
@@ -70,20 +70,18 @@ $(function() {
     var month = null;
     var r = [];
 
-    var i = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
-    var s = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    var monthArr = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+    var weekdayArr = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
-    var calendar = $("#calendar");
-    var toolbar = calendar.find(".header");
-
-    var weekdaysEl = calendar.find(".weekdays");
-    var contentEl = calendar.find(".content");
+    var calendarEl = $("#calendar");
+    var toolbarEl = calendarEl.find(".header");
+    var weekdaysEl = calendarEl.find(".weekdays");
+    var contentEl = calendarEl.find(".content");
 
     b();
     days();
 
-    toolbar.find('.icon').on("click", function() {
-        var element = $(this);
+    toolbarEl.find('.icon').on("click", function() {
         var set = function(dir) {
             month = (dir == "next") ? month + 1 : month - 1;
             if(month < 1) {
@@ -95,7 +93,7 @@ $(function() {
             }
             days();
         };
-        if(element.hasClass("prev")) {
+        if($(this).hasClass("prev")) {
             set("prev");
         } else {
             set("next");
