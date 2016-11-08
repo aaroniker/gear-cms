6d5a5533a29ebd0479559238d891ca1636371866
Aaron Iker <aaron.iker@web.de>
2016-11-08 17:59:46 +0100

    remove dev status, settings (edit config.json instead)

--- admin/view/system/settings.php
+++ admin/view/system/settings.php
@@ -13,38 +13,32 @@
         $form->addTab(lang::get('general'));
 
         $field = $form->addTextField('sitename', option::get('sitename'));
-	    $field->fieldName(lang::get('sitename'));
+        $field->fieldName(lang::get('sitename'));
         $field->fieldValidate();
 
         $form->addTab(lang::get('advanced'));
 
         $field = $form->addTextField('siteurl', config::get('url'));
-	    $field->fieldName(lang::get('siteurl'));
+        $field->fieldName(lang::get('siteurl'));
         $field->fieldValidate();
 
-        $field = $form->addSwitchField('devStatus', config::get('dev'));
-	    $field->fieldName(lang::get('status'));
-        $field->add(true, lang::get('development'));
-
         $field = $form->addSwitchField('cache', config::get('cache'));
-	    $field->fieldName(lang::get('cache'));
+        $field->fieldName(lang::get('cache'));
         $field->add(true, lang::get('yes'));
 
         $field = $form->addSwitchField('navShowUL', option::get('navShowUL', false));
-	    $field->fieldName(lang::get('dropdown_nav'));
+        $field->fieldName(lang::get('dropdown_nav'));
         $field->add(1, lang::get('show'));
 
         if($form->isSubmit()) {
 
             if($form->validation()) {
 
-			    $array = $form->getAll();
+                $array = $form->getAll();
 
-                $dev = ($array['devStatus']) ? true : false;
                 $cache = ($array['cache']) ? true : false;
 
                 config::add('url', $array['siteurl'], true);
-                config::add('dev', $dev, true);
                 config::add('cache', $cache, true);
                 config::save();
 
@@ -55,11 +49,11 @@
 
                 url::refresh();
 
-		    } else {
-			    $form->getErrors();
-		    }
+            } else {
+                $form->getErrors();
+            }
 
-	    }
+        }
 
     ?>
 
