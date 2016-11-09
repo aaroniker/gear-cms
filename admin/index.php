cea9db0954de121313384e301bf6bfc03482ea1f
Aaron Iker <aaron.iker@web.de>
2016-11-09 23:25:55 +0100

    add jquery ui 1.11.4

--- admin/index.php
+++ admin/index.php
@@ -4,13 +4,14 @@
     include($base.'gear/bootstrap.php');
 
     theme::addCSS('https://cdn.jsdelivr.net/animatecss/3.5.2/animate.min.css');
+    theme::addCSS('https://cdn.jsdelivr.net/jquery.ui/1.11.4/jquery-ui.structure.min.css');
     theme::addCSS(url::assets('css/style.css'));
 
     theme::addJS('https://cdn.jsdelivr.net/jquery/3.1.1/jquery.min.js');
+    theme::addJS('https://cdn.jsdelivr.net/jquery.ui/1.11.4/jquery-ui.min.js');
     theme::addJS('https://cdn.jsdelivr.net/lodash/4.16.6/lodash.min.js');
     theme::addJS('https://cdn.jsdelivr.net/highlight.js/9.6.0/highlight.min.js');
     theme::addJS('https://cdn.jsdelivr.net/vue/2.0.5/vue.js');
-    theme::addJS(url::assets('js/dragula.js'));
     theme::addJS(url::assets('js/form.js'));
     theme::addJS(url::assets('js/session.js'));
     theme::addJS(url::assets('js/sortable.js'));
