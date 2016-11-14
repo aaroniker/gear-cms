ab43ab097dc9355647f27cabcf5e5531395d0624
Aaron Iker <aaron.iker@web.de>
2016-11-14 19:15:42 +0100

    content/list update

--- admin/view/content/list.php
+++ admin/view/content/list.php
@@ -1,74 +1,61 @@
-<section id="content">
+<?php
+    admin::addButton('
+        <a @click="addPageModal = true" class="button">
+            '.lang::get('add').'
+        </a>
+    ');
 
-    <header>
-
-        <h2 v-html="headline"></h2>
-
-        <nav>
-            <ul>
-                <li>
-                    <a @click="addPageModal = true" class="button">
-                        <?=lang::get('add'); ?>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-
-    </header>
-
-    <?php
-
-        $form = new form();
-        $form->setHorizontal(false);
-
-        $form->addFormAttribute('v-on:submit.prevent', 'addPage');
-
-        $field = $form->addTextField('name', '');
-        $field->fieldName(lang::get('name'));
-        $field->addAttribute('v-model', 'pageName');
-        $field->fieldValidate();
-
-        $field = $form->addRawField('<searchbox :list="pageAll" val="name" id="id"></searchbox>');
-        $field->fieldName(lang::get('page_parent'));
-
-    ?>
-
-    <modal v-if="addPageModal" @close="addPageModal = false">
-        <h3 slot="header"><?=lang::get('add'); ?></h3>
-        <div slot="content">
-            <?=$form->show(); ?>
-        </div>
-    </modal>
-
-    <div id="pageList" class="box">
-        <h3><?=option::get('sitename'); ?></h3>
-        <ul>
-            <item v-for="model in pageTree" :model="model"></item>
-        </ul>
-        <template v-if="!pageTreeLength">
-            <?=lang::get('no_results'); ?>
-        </template>
-    </div>
-
-</section>
-
-<template id="item-template">
-    <li>
-        <div class="entry clear">
-            <div class="info" :data-id="model.id">
-                <span>{{ model.name }}</span>
-                <small>{{ model.siteURL }}</small>
+    admin::addComponent('
+    <template id="item-template">
+        <li>
+            <div class="entry clear">
+                <div class="info" :data-id="model.id">
+                    <span>{{ model.name }}</span>
+                    <small>{{ model.siteURL }}</small>
+                </div>
+                <span v-if="model.home" class="setHome" data-tooltip="'.lang::get('page_home').'"><i class="icon icon-ios-home"></i></span>
+                <span v-else class="setHome" @click="setHome(model.id)"><i class="inactive icon icon-ios-home-outline"></i></span>
+                <a :href="\''.url::admin('content', ['index', 'delete']).'/\' + model.id" class="icon delete ajax icon-ios-trash-outline"></a>
+                <a :href="\''.url::admin('content', ['index', 'edit']).'/\' + model.id" class="icon edit icon-edit"></a>
             </div>
-            <span v-if="model.home" class="setHome" data-tooltip="<?=lang::get('page_home'); ?>"><i class="icon icon-ios-home"></i></span>
-            <span v-else class="setHome" @click="setHome(model.id)"><i class="inactive icon icon-ios-home-outline"></i></span>
-            <a :href="'<?=url::admin('content', ['index', 'delete']); ?>/' + model.id" class="icon delete ajax icon-ios-trash-outline"></a>
-            <a :href="'<?=url::admin('content', ['index', 'edit']); ?>/' + model.id" class="icon edit icon-edit"></a>
-        </div>
-        <ul v-if="model.children">
-            <item v-for="model in model.children" :model="model"></item>
-        </ul>
-    </li>
-</template>
+            <ul v-if="model.children">
+                <item v-for="model in model.children" :model="model"></item>
+            </ul>
+        </li>
+    </template>
+    ');
+
+    $form = new form();
+    $form->setHorizontal(false);
+
+    $form->addFormAttribute('v-on:submit.prevent', 'addPage');
+
+    $field = $form->addTextField('name', '');
+    $field->fieldName(lang::get('name'));
+    $field->addAttribute('v-model', 'pageName');
+    $field->fieldValidate();
+
+    $field = $form->addRawField('<searchbox :list="pageAll" val="name" id="id"></searchbox>');
+    $field->fieldName(lang::get('page_parent'));
+
+?>
+
+<modal v-if="addPageModal" @close="addPageModal = false">
+    <h3 slot="header"><?=lang::get('add'); ?></h3>
+    <div slot="content">
+        <?=$form->show(); ?>
+    </div>
+</modal>
+
+<div id="pageList" class="box">
+    <h3><?=option::get('sitename'); ?></h3>
+    <ul>
+        <item v-for="model in pageTree" :model="model"></item>
+    </ul>
+    <template v-if="!pageTreeLength">
+        <?=lang::get('no_results'); ?>
+    </template>
+</div>
 
 <?php
 theme::addJSCode('
@@ -86,7 +73,7 @@ theme::addJSCode('
     });
 
     new Vue({
-        el: "#content",
+        el: "#app",
         data: {
             headline: lang["pages"],
             addPageModal: false,
