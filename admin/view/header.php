aa7a520001748e4fc445ef9d91644a6f6a639c67
Aaron Iker <aaron.iker@web.de>
2016-11-14 18:01:52 +0100

    scrollable menu sidebar / mobile

--- admin/view/header.php
+++ admin/view/header.php
@@ -1,111 +1,115 @@
 <div id="left">
+    <div class="inner">
+        <div class="wrap">
 
-    <div class="top clear">
-        <a class="logo" href="<?=url::admin('dashboard'); ?>">
-            <img src="<?=url::assets('img/logo.svg'); ?>" alt="Gear Logo">
-        </a>
-    </div>
-
-    <div class="user">
-        <div class="inner clear">
-            <a href="<?=url::admin('user', ['index', 'edit']); ?>" class="profile">
-                <?php
-
-                    if(user::current()->avatar && file_exists(dir::media(user::current()->avatar))) {
-                        echo '<div class="img" style="background-image: url('.url::media(user::current()->avatar).');"></div>';
-                    } else {
-                        echo user::getAvatar(36, true);
-                    }
-
-                ?>
-                <span><?=user::current()->username; ?></span>
+        <div class="top clear">
+            <a class="logo" href="<?=url::admin('dashboard'); ?>">
+                <img src="<?=url::assets('img/logo.svg'); ?>" alt="Gear Logo">
             </a>
+        </div>
+
+        <div class="user">
+            <div class="inner clear">
+                <a href="<?=url::admin('user', ['index', 'edit']); ?>" class="profile">
+                    <?php
+
+                        if(user::current()->avatar && file_exists(dir::media(user::current()->avatar))) {
+                            echo '<div class="img" style="background-image: url('.url::media(user::current()->avatar).');"></div>';
+                        } else {
+                            echo user::getAvatar(36, true);
+                        }
+
+                    ?>
+                    <span><?=user::current()->username; ?></span>
+                </a>
+                <nav>
+                    <ul class="clear">
+                        <li>
+                            <a href="?logout=1" data-tooltip="<?=lang::get('logout'); ?>">
+                                <i class="icon icon-log-out"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <?php
+            $menu = admin::getMenu();
+            if($menu):
+        ?>
+        <nav id="nav">
+            <ul>
+            <?php
+                foreach($menu as $url => $array):
+
+                    $openArr = [];
+                    if(user::current()->openMenu) {
+                        $openArr = unserialize(user::current()->openMenu);
+                    }
+                    $open = (isset($openArr[$url])) ? true : false;
+
+                    $class = ($array['class']) ? $array['class'] : '';
+                    $class = ($open) ? $class.' drop dropFade' : $class;
+
+                    $sub = admin::getSubmenu($url);
+                    $drop = ($sub && count($sub) > 1) ? '<span data-id="'.$url.'"></span>' : '';
+
+                    echo '
+                    <li class="'.$class.'">
+                        '.$drop.'
+                        <a href="'.url::admin($url).'">
+                            <i class="icon icon-'.$array['icon'].'"></i>
+                            '.$array['name'].'
+                        </a>
+                    ';
+                    if($sub && count($sub) > 1):
+                        echo '<ul>';
+
+                        foreach($sub as $url => $array):
+
+                            $class = ($array['class']) ? ' class="'.$array['class'].'"' : '';
+
+                            echo '
+                            <li'.$class.'>
+                                <a href="'.url::admin($url).'">'.$array['name'].'</a>
+                            </li>
+                            ';
+
+                        endforeach;
+
+                        echo '</ul>';
+                    endif;
+
+                    echo '
+                    </li>
+                    ';
+
+                endforeach;
+            ?>
+            </ul>
+        </nav>
+        <?php
+            endif;
+        ?>
+
+        <div class="info clear">
+            <div class="fl-left">
+                <?=sprintf(lang::get('version'), config::get('version')); ?>
+            </div>
             <nav>
                 <ul class="clear">
                     <li>
-                        <a href="?logout=1" data-tooltip="<?=lang::get('logout'); ?>">
-                            <i class="icon icon-log-out"></i>
+                        <a href="http://gearcms.org" data-tooltip="<?=lang::get('website'); ?>" target="_blank">
+                            <i class="icon icon-earth"></i>
                         </a>
                     </li>
                 </ul>
             </nav>
         </div>
+
     </div>
-
-    <?php
-        $menu = admin::getMenu();
-        if($menu):
-    ?>
-    <nav id="nav">
-        <ul>
-        <?php
-            foreach($menu as $url => $array):
-
-                $openArr = [];
-                if(user::current()->openMenu) {
-                    $openArr = unserialize(user::current()->openMenu);
-                }
-                $open = (isset($openArr[$url])) ? true : false;
-
-                $class = ($array['class']) ? $array['class'] : '';
-                $class = ($open) ? $class.' drop dropFade' : $class;
-
-                $sub = admin::getSubmenu($url);
-                $drop = ($sub && count($sub) > 1) ? '<span data-id="'.$url.'"></span>' : '';
-
-                echo '
-                <li class="'.$class.'">
-                    '.$drop.'
-                    <a href="'.url::admin($url).'">
-                        <i class="icon icon-'.$array['icon'].'"></i>
-                        '.$array['name'].'
-                    </a>
-                ';
-                if($sub && count($sub) > 1):
-                    echo '<ul>';
-
-                    foreach($sub as $url => $array):
-
-                        $class = ($array['class']) ? ' class="'.$array['class'].'"' : '';
-
-                        echo '
-                        <li'.$class.'>
-                            <a href="'.url::admin($url).'">'.$array['name'].'</a>
-                        </li>
-                        ';
-
-                    endforeach;
-
-                    echo '</ul>';
-                endif;
-
-                echo '
-                </li>
-                ';
-
-            endforeach;
-        ?>
-        </ul>
-    </nav>
-    <?php
-        endif;
-    ?>
-
-    <div class="info clear">
-        <div class="fl-left">
-            <?=sprintf(lang::get('version'), config::get('version')); ?>
-        </div>
-        <nav>
-            <ul class="clear">
-                <li>
-                    <a href="http://gearcms.org" data-tooltip="<?=lang::get('website'); ?>" target="_blank">
-                        <i class="icon icon-earth"></i>
-                    </a>
-                </li>
-            </ul>
-        </nav>
     </div>
-
 </div>
 
 <header id="head" class="clear">
