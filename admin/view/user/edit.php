9b7162f211cb6c9089d2d3737eb9d4789ab366bc
Aaron Iker <aaron.iker@web.de>
2016-11-14 19:49:00 +0100

    user/edit update

--- admin/view/user/edit.php
+++ admin/view/user/edit.php
@@ -1,117 +1,103 @@
-<section id="user">
+<?php
+    admin::addButton('
+        <a href="'.url::admin('user').'" class="button border">
+            '.lang::get('back').'
+        </a>
+    ');
 
-    <header>
+    $form = new form();
 
-        <h2><?=lang::get('edit'); ?></h2>
+    $field = $form->addTextField('username', $this->model->username);
+	$field->fieldName(lang::get('username'));
+    $field->fieldValidate();
 
-        <nav>
-            <ul>
-                <li>
-                    <a href="<?=url::admin('user'); ?>" class="button border">
-                        <?=lang::get('back'); ?>
-                    </a>
-                </li>
-            </ul>
-        </nav>
+    $field = $form->addMediaField('avatar', $this->model->avatar, ['ext' => 'jpg,jpeg,png,gif']);
+    $field->fieldName(lang::get('avatar'));
 
-    </header>
+    $field = $form->addTextField('email', $this->model->email);
+	$field->fieldName(lang::get('email'));
+    $field->fieldValidate('valid_email|required');
 
-    <?php
+    if($this->model->id != user::current()->id) {
 
-        $form = new form();
+    	$field = $form->addRadioInlineField('status', $this->model->status);
+        $field->add(1, lang::get('active'));
+        $field->add(0, lang::get('blocked'));
 
-        $field = $form->addTextField('username', $this->model->username);
-	    $field->fieldName(lang::get('username'));
-        $field->fieldValidate();
+    } else {
+        $field = $form->addRawField('<p class="static">'.lang::get('status_change_own').'</p>');
+    }
 
-        $field = $form->addMediaField('avatar', $this->model->avatar, ['ext' => 'jpg,jpeg,png,gif']);
-        $field->fieldName(lang::get('avatar'));
+    $field->fieldName(lang::get('status'));
 
-        $field = $form->addTextField('email', $this->model->email);
-	    $field->fieldName(lang::get('email'));
-        $field->fieldValidate('valid_email|required');
+    $field = $form->addRawField('
+        <div id="pwChange">
+            <a v-if="!changePassword" @click="changePassword = true" class="button border">'.lang::get('change_password').'</a>
+            <template v-if="changePassword">
+                <a @click="changePassword = false" class="button border">'.lang::get('close').'</a>
+                <a @click="generate" class="button border">
+                    <i class="icon icon-refresh"></i>
+                    '.lang::get('generate_password').'
+                </a>
+                <input v-model="newPassword" type="text" name="password" id="passwordForm" class="form-field">
+            </template>
+        </div>
+    ');
+    $field->fieldName(lang::get('password'));
 
-        if($this->model->id != user::current()->id) {
+    $form->addSave('password');
 
-    	    $field = $form->addRadioInlineField('status', $this->model->status);
-            $field->add(1, lang::get('active'));
-            $field->add(0, lang::get('blocked'));
+    $field = $form->addSelectField('permissionID', $this->model->permissionID);
+    $field->fieldName(lang::get('permissions'));
+    $field->add(0, lang::get('admin'));
 
-        } else {
-    	    $field = $form->addRawField('<p class="static">'.lang::get('status_change_own').'</p>');
-        }
+    foreach(PermissionModel::getAllFromDb() as $entry) {
+        $field->add($entry->id, $entry->name);
+    }
 
-        $field->fieldName(lang::get('status'));
+    if($this->model->id == user::current()->id) {
+        $field->addAttribute('disabled');
+    }
 
-        $field = $form->addRawField('
-            <div id="pwChange">
-                <a v-if="!changePassword" @click="changePassword = true" class="button border">'.lang::get('change_password').'</a>
-                <template v-if="changePassword">
-                    <a @click="changePassword = false" class="button border">'.lang::get('close').'</a>
-                    <a @click="generate" class="button border">
-                        <i class="icon icon-refresh"></i>
-                        '.lang::get('generate_password').'
-                    </a>
-                    <input v-model="newPassword" type="text" name="password" id="passwordForm" class="form-field">
-                </template>
-            </div>
-        ');
-        $field->fieldName(lang::get('password'));
+    if($form->isSubmit()) {
 
-        $form->addSave('password');
+        if($form->validation()) {
 
-        $field = $form->addSelectField('permissionID', $this->model->permissionID);
-        $field->fieldName(lang::get('permissions'));
-        $field->add(0, lang::get('admin'));
+		    extension::add('model_beforeSave', function($data) {
+                $data['avatar'] = ($data['avatar']) ? $data['avatar'] : null;
+                if(!empty($data['password'])) {
+    		        $password = password_hash($data['password'], PASSWORD_DEFAULT);
+    		        $data['password'] = $password;
+                } else {
+                    unset($data['password']);
+                }
+    		    return $data;
+		    });
 
-        foreach(PermissionModel::getAllFromDb() as $entry) {
-            $field->add($entry->id, $entry->name);
-        }
+			$this->model->save($form->getAll(), true);
 
-        if($this->model->id == user::current()->id) {
-            $field->addAttribute('disabled');
-        }
+            message::success(lang::get('user_edited'));
 
-        if($form->isSubmit()) {
-
-            if($form->validation()) {
-
-			    extension::add('model_beforeSave', function($data) {
-                    $data['avatar'] = ($data['avatar']) ? $data['avatar'] : null;
-                    if(!empty($data['password'])) {
-    			        $password = password_hash($data['password'], PASSWORD_DEFAULT);
-    			        $data['password'] = $password;
-                    } else {
-                        unset($data['password']);
-                    }
-    		        return $data;
-			    });
-
-			    $this->model->save($form->getAll(), true);
-
-                message::success(lang::get('user_edited'));
-
-		    } else {
-			    $form->getErrors();
-		    }
-
+		} else {
+		    $form->getErrors();
 	    }
 
-    ?>
+	}
 
-    <div class="columns">
-        <div class="md-9 lg-7">
-            <?=$form->show(); ?>
-        </div>
+?>
+
+<div class="columns">
+    <div class="md-9 lg-7">
+        <?=$form->show(); ?>
     </div>
-
-</section>
+</div>
 
 <?php
 theme::addJSCode('
     new Vue({
-        el: "#pwChange",
+        el: "#app",
         data: {
+            headline: "'.lang::get('edit').'",
             newPassword: "",
             changePassword: false
         },
