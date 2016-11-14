3040cc642dcd6d2c90d9a0e8a18f8c0832b5837d
Aaron Iker <aaron.iker@web.de>
2016-11-14 19:48:15 +0100

    blocks/show update

--- admin/view/extensions/blocks/show.php
+++ admin/view/extensions/blocks/show.php
@@ -1,47 +1,43 @@
-<section id="block">
+<?php
+    admin::addButton('
+        <a href="'.url::admin('extensions', ['blocks']).'" class="button border">
+            '.lang::get('back').'
+        </a>
+    ');
 
-    <header>
+    $form = new form();
+    $form->setShowSubmit(false);
 
-        <h2><?=$block->getInfo('name'); ?></h2>
+    $form->addTab(lang::get('info'));
+    $form->addRawField('
+        <pre><code>'.json_encode($block->getInfo(), JSON_PRETTY_PRINT).'</code></pre>
+    ');
 
-        <nav>
-            <ul>
-                <li>
-                    <a href="<?=url::admin('extensions', ['blocks']); ?>" class="button border">
-                        <?=lang::get('back'); ?>
-                    </a>
-                </li>
-            </ul>
-        </nav>
+    $form->addTab(lang::get('content'));
+    $form->addRawField('
+        <pre><code>'.htmlentities($block->getContent()).'</code></pre>
+    ');
 
-    </header>
+    $form->addTab(lang::get('css'));
+    $form->addRawField('
+        <pre><code>'.$block->getCSS().'</code></pre>
+    ');
 
-    <?php
+?>
 
-        $form = new form();
-        $form->setShowSubmit(false);
-
-        $form->addTab(lang::get('info'));
-        $form->addRawField('
-            <pre><code>'.json_encode($block->getInfo(), JSON_PRETTY_PRINT).'</code></pre>
-        ');
-
-        $form->addTab(lang::get('content'));
-        $form->addRawField('
-            <pre><code>'.htmlentities($block->getContent()).'</code></pre>
-        ');
-
-        $form->addTab(lang::get('css'));
-        $form->addRawField('
-            <pre><code>'.$block->getCSS().'</code></pre>
-        ');
-
-    ?>
-
-    <div class="columns">
-        <div class="md-9 lg-7">
-            <?=$form->show(); ?>
-        </div>
+<div class="columns">
+    <div class="md-9 lg-7">
+        <?=$form->show(); ?>
     </div>
+</div>
 
-</section>
+<?php
+theme::addJSCode('
+    new Vue({
+        el: "#app",
+        data: {
+            headline: "'.$block->getInfo('name').'"
+        }
+    });
+');
+?>
