f7b23ce28eeb1fd0ce5c564cd722a5aa4783dbc9
Aaron Iker <aaron.iker@web.de>
2016-11-18 11:47:07 +0100

    lang class method getAll()

--- gear/classes/lang.php
+++ gear/classes/lang.php
@@ -4,6 +4,7 @@ class lang {
 
     static $lang;
     static $langArray = [];
+    static $allLangs = [];
 
     static public function setLang($lang = 'de') {
 
@@ -36,6 +37,30 @@ class lang {
         self::$langArray = array_merge((array)$array, self::$langArray);
 
         return self::$langArray = $array;
+
+    }
+
+    static public function getAll() {
+
+        if(!count(self::$allLangs)) {
+
+            $langs = array_diff(scandir(dir::language()), ['.', '..']);
+
+            foreach($langs as $lang) {
+
+                $lang = str_replace('.json', '', $lang);
+
+                $file = file_get_contents(dir::language($lang.'.json'));
+                $file = preg_replace("/#\s*([a-zA-Z ]*)/", "", $file);
+                $array = json_decode($file, true);
+
+                self::$allLangs[$lang] = $array['lang'];
+
+            }
+
+        }
+
+        return self::$allLangs;
 
     }
 
