945a31a3d82bd87fc993e89f90a9242039a04ee2
Aaron Iker <aaron.iker@web.de>
2016-11-22 00:40:08 +0100

    content/edit grid layout WIP

--- admin/view/content/edit.php
+++ admin/view/content/edit.php
@@ -6,14 +6,55 @@
     ');
 ?>
 
-content
+<div class="row" v-for="(columns, row) in grid">
+    <div class="columns">
+        <div v-for="(column, key) in columns" :class="breakpoint + '-' + column.size">
+            <div class="box">
+                <div class="size">
+                    <i v-if="column.size < 12" @click="size(row, key, 1)" class="plus icon icon-plus-circled"></i>
+                    <i v-if="column.size > 1" @click="size(row, key, -1)" class="minus icon icon-minus-circled"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php
 theme::addJSCode('
     new Vue({
         el: "#app",
         data: {
-            headline: "'.$this->model->name.'"
+            headline: "'.$this->model->name.'",
+            breakpoint: "md",
+            grid: [
+                [
+                    {
+                        size: "6"
+                    },
+                    {
+                        size: "3"
+                    },
+                    {
+                        size: "3"
+                    }
+                ],
+                [
+                    {
+                        size: "4"
+                    },
+                    {
+                        size: "8"
+                    }
+                ]
+            ]
+        },
+        created: function() {
+            var vue = this;
+        },
+        methods: {
+            size: function(row, key, num) {
+                this.grid[row][key].size = parseInt(this.grid[row][key].size) + parseInt(num);
+            }
         }
     });
 ');
