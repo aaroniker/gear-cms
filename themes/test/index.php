2a3509136ac57a2badfc34a7be7e1c5cff59adf1
Aaron Iker <aaron.iker@web.de>
2016-11-29 15:28:27 +0100

    theme index.php {{ content }} var

---
+++ themes/test/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+<title>Test Theme</title>
+
+</head>
+
+<body>
+
+{{ content|raw }}
+
+</body>
+</html>
