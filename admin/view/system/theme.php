d7f23135c35c71720b2829977e5da9784ad4f02c
Aaron Iker <aaron.iker@web.de>
2016-12-14 18:09:41 +0100

    system/themes toggleInfo (des, vers, url, author)

--- admin/view/system/theme.php
+++ admin/view/system/theme.php
@@ -2,8 +2,17 @@
     <div class="columns">
         <div v-for="(entry, key) in themes" class="md-4 sm-6">
             <div class="box">
-                <h3>{{ entry.name }}</h3>
-                <div class="screenshot" :style="'background-image: url(<?=config::get('url').'themes/'; ?>' + key + '/screenshot.png);'"></div>
+                <div class="clear">
+                    <h3>{{ entry.name }}</h3>
+                    <i @click="toggleInfo(key)" :class="{ active: _.includes(info, key) }" class="info icon icon-information-circled"></i>
+                </div>
+                <div class="screenshot" :style="'background-image: url(<?=config::get('url').'themes/'; ?>' + key + '/screenshot.png);'">
+                    <div v-show="_.includes(info, key)">
+                        {{ entry.description }}
+                        <span>{{ entry.version }}</span>
+                        <a v-if="entry.author" :href="entry.url" target="_blank">{{ entry.author }}</a>
+                    </div>
+                </div>
                 <div v-if="entry.active" class="button active"><?=lang::get('active'); ?></div>
                 <a v-else class="button border" @click="setActive(key)"><?=lang::get('activate'); ?></a>
             </div>
@@ -17,6 +26,7 @@ theme::addJSCode('
         el: "#app",
         data: {
             headline: lang["theme"],
+            info: [],
             themes: '.json_encode(theme::getAll()).'
         },
         created: function() {
@@ -43,6 +53,17 @@ theme::addJSCode('
                         vue.fetch();
                     }
                 });
+
+            },
+            toggleInfo: function(theme) {
+
+                var vue = this;
+
+                if(_.includes(this.info, theme)) {
+                    this.info.splice(this.info.indexOf(theme),1);
+                } else {
+                    this.info.push(theme);
+                }
 
             },
             fetch: function() {
