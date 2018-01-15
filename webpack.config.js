9bea10c0b242289411285b6e49efe19a4fc68d8d
Aaron Iker <mail@aaroniker.me>
2018-01-15 16:56:37 +0100

    add extensions dir

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 var glob = require('glob');
 var exports = [];
 
-glob.sync('{gear/modules/**,gear/installer/**,gear/backend/**,themes/**}/webpack.config.js', {ignore: '**/node_modules/*'}).forEach(function(file) {
+glob.sync('{gear/modules/**,gear/installer/**,gear/backend/**,extensions/**,themes/**}/webpack.config.js', {ignore: '**/node_modules/*'}).forEach(function(file) {
     var dir = path.join(__dirname, path.dirname(file));
     exports = exports.concat(require('./' + file).map(function(config) {
         return _.merge({context: dir, output: {path: dir}}, config);
