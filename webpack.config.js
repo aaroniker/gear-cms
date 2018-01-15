016264a05debdc81310755acde50a458ccf71f0c
Aaron Iker <mail@aaroniker.me>
2018-01-15 17:13:18 +0100

    tidy up main webpack.config.js, exit if no modules found

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,23 @@ var path = require('path');
 var glob = require('glob');
 var exports = [];
 
-glob.sync('{gear/modules/**,gear/installer/**,gear/backend/**,extensions/**,themes/**}/webpack.config.js', {ignore: '**/node_modules/*'}).forEach(function(file) {
+glob.sync('{gear/modules/**,gear/installer/**,gear/backend/**,extensions/**,themes/**}/webpack.config.js', {
+    ignore: '**/node_modules/*'
+}).forEach(function(file) {
     var dir = path.join(__dirname, path.dirname(file));
     exports = exports.concat(require('./' + file).map(function(config) {
-        return _.merge({context: dir, output: {path: dir}}, config);
+        return _.merge({
+            context: dir,
+            output: {
+                path: dir
+            }
+        }, config);
     }));
 });
+
+if(!exports.length) {
+    console.log('No modules with a "webpack.config.js" found.');
+    process.exit(1);
+}
 
 module.exports = exports;
