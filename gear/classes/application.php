6cf2a2242077e928a1494b73df5beb15aaab4eb1
Aaron Iker <mail@aaroniker.me>
2018-01-18 21:26:56 +0100

    app debug option

--- gear/classes/application.php
+++ gear/classes/application.php
@@ -4,15 +4,19 @@ class application {
 
     public function __construct($config) {
 
-        $this->hook = new hook();
         $this->config = $config;
+
+        if($this->config->get('system')['debug']) {
+            ini_set('display_errors', 1);
+            error_reporting(E_ALL);
+        }
+
+        $this->hook = new hook();
         $this->modules = new moduleManager($this);
 
     }
 
     public function boot() {
-
-        ini_set('display_errors', $this->config->get('system')['debug'] ? 1 : 0);
 
         $this->hook::run('boot', $this);
 
