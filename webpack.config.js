31ab53b4973784bc6ad9a257eda3a9c58b50019c
Aaron Iker <mail@aaroniker.me>
2018-03-26 02:18:09 +0200

    webpack 4 update

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ var _ = require('lodash');
 var webpack = require('webpack');
 var path = require('path');
 var glob = require('glob');
-var MinifyPlugin = require("babel-minify-webpack-plugin");
 var exports = [];
 
 glob.sync('{gear/modules/**,gear/installer/**,gear/system/**,extensions/**,themes/**}/webpack.config.js', {
@@ -15,19 +14,8 @@ glob.sync('{gear/modules/**,gear/installer/**,gear/system/**,extensions/**,theme
             output: {
                 path: dir
             },
-            plugins: [
-                new webpack.DefinePlugin({
-                    'process.env': {
-                        NODE_ENV: '"production"'
-                    }
-                }),
-                new MinifyPlugin(),
-                new webpack.LoaderOptionsPlugin({
-                    minimize: true
-                })
-            ],
             module: {
-                loaders: [
+                rules: [
                     {
                         test: /\.vue$/,
                         loader: 'vue-loader',
