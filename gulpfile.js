d899d613f79d4112258206acdc88ad2ec8748f79
Aaron Iker <mail@aaroniker.me>
2019-01-05 12:05:24 +0100

    update package json, change gulpfile for 4.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,16 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 
-gulp.task('default', ['styles']);
-
 gulp.task('styles', function() {
-    gulp.src(['**/modules/**/style.scss'])
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(rename(function(file) {
-            file.dirname += "/dist";
-        }))
-        .pipe(gulp.dest('.'))
+    return gulp.src(['**/modules/**/style.scss']).pipe(sass({
+        outputStyle: 'compressed'
+    })).pipe(rename(function(file) {
+        file.dirname += "/dist";
+    })).pipe(gulp.dest('.'))
 });
 
 gulp.task('watch', function(cb) {
-    gulp.watch('**/modules/**/*.scss', ['styles']);
+    return gulp.watch('**/modules/**/*.scss', gulp.series('styles'));
 });
+
+gulp.task('default', gulp.series('styles'));
